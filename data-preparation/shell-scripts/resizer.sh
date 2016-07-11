e284ce6e88220883a33504568462b428d40bfc49
Jittawe Tandhanand <jittawe@itorama.com>
2016-07-11 17:19:48 +0700

    updated resizer.sh to run on production

--- data-preparation/shell-scripts/resizer.sh
+++ data-preparation/shell-scripts/resizer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-SOURCE="/home/gadgeteer/mol/images/"
-ORIGINAL="/home/gadgeteer/Workspace/mol/web/code/plugins/http/public/images/products/original"
-THUMBNAIL="/home/gadgeteer/Workspace/mol/web/code/plugins/http/public/images/products/thumbnail"
+SOURCE="/media/mol/"
+ORIGINAL="/var/www/mol/web/code/plugins/http/public/images/products/original"
+THUMBNAIL="/var/www/mol/web/code/plugins/http/public/images/products/thumbnail"
 
 PROCESS=0
 PROCESS=`ps | grep resizer.sh |grep -v grep |wc -l`
 echo $PROCESS
 if [ $PROCESS -le 2 ]; then
   echo "Process is about to run."
-  
+
   # sync folder structure
   # rsync -a -f "+ */" -f "- *" $SOURCE $ORIGINAL
   # rsync -a -f "+ */" -f "- *" $SOURCE $THUMBNAIL
