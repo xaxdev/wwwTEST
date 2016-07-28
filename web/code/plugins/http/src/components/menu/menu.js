ad21c83728cfb5908819fde45ba85b1397734f16
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2016-07-28 11:49:48 +0700

    Fixed class active menu

--- web/code/plugins/http/src/components/menu/menu.js
+++ web/code/plugins/http/src/components/menu/menu.js
@@ -3,8 +3,12 @@ import { Link } from 'react-router';
 import { Navbar,Nav,NavDropdown,MenuItem,NavItem } from 'react-bootstrap';
 
 const Menu =  () => {
+  const url = window.location.href;
+  const countLastPath = url.split('/').length - 1;
+  const lastPath = url.split('/')[countLastPath];
+
   const { role } = JSON.parse(sessionStorage.logindata);
-  const UserManagement = role == 'Admin'? <NavItem href="/users">User Management</NavItem> : '';
+  const UserManagement = role == 'Admin'? <NavItem href="/users" className={`${(lastPath == 'users')?'active':''}`}>User Management</NavItem> : '';
   return(
 <Navbar inverse>
     <Navbar.Header>
@@ -12,7 +16,7 @@ const Menu =  () => {
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav className="padding-lf30">
-        <NavItem href="/inventories" className="active">Inventory Report</NavItem>
+        <NavItem href="/inventories" className={`${(lastPath == 'inventories')?'active':''}`}>Inventory Report</NavItem>
         {/*<NavItem href="#">My Catalog</NavItem>*/}
         {/*<NavDropdown  title="Download" id="basic-nav-dropdown">
           <MenuItem >Download</MenuItem>
