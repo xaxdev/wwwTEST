462aa4ab77d7709643ad0f28c34e5af86229a543
nopparit.paksee@itorama.com <nopparit.paksee@itorama.com>
2016-07-28 12:15:11 +0700

    fix active menu

--- web/code/plugins/http/src/components/menu/menu.js
+++ web/code/plugins/http/src/components/menu/menu.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router';
 import { Navbar,Nav,NavDropdown,MenuItem,NavItem } from 'react-bootstrap';
 
-const Menu =  () => {
+const Menu =  (props) => {
   const url = window.location.href;
   const countLastPath = url.split('/').length - 1;
   const lastPath = url.split('/')[countLastPath];
-
   const { role } = JSON.parse(sessionStorage.logindata);
-  const UserManagement = role == 'Admin'? <NavItem href="/users" className={`${(lastPath == 'users')?'active':''}`}>User Management</NavItem> : '';
+  const UserManagement = role == 'Admin'? <NavItem href="/users" className={`${(props.currentLocation == '/users' || props.currentLocation == '/user')?'active':''}`}>User Management</NavItem> : '';
+  console.log(props.currentLocation);
   return(
 <Navbar inverse>
     <Navbar.Header>
@@ -16,7 +16,7 @@ const Menu =  () => {
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav className="padding-lf30">
-        <NavItem href="/inventories" className={`${(lastPath == 'inventories')?'active':''}`}>Inventory Report</NavItem>
+        <NavItem href="/inventories" className={`${(props.currentLocation !== '/users' && props.currentLocation !== '/user')?'active':''}`}>Inventory Report</NavItem>
         {/*<NavItem href="#">My Catalog</NavItem>*/}
         {/*<NavDropdown  title="Download" id="basic-nav-dropdown">
           <MenuItem >Download</MenuItem>
