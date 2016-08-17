426409e0c51cabfbabc8317f23e6bfc2086676e7
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2016-08-17 17:44:07 +0700

    Changed data at tree data and sql hierarchy

--- data-migration/query/hierarchy.sql
+++ data-migration/query/hierarchy.sql
@@ -1,15 +1,21 @@
-SELECT ROW_NUMBER() OVER(ORDER BY [ITO_CATEGORYPATH]) AS [id]
-      ,[NAME] AS 'name'
-      ,[ITO_PARENTCATEGORYPATH] AS 'parent'
-      ,[ITO_CATEGORYPATH] AS 'path'
-      ,CASE
-          WHEN [ITO_CATEGORYPATH] LIKE '%Jewelry%' THEN 'Jewelry'
-          WHEN [ITO_CATEGORYPATH] LIKE '%Stone%' THEN 'Stone'
-          WHEN [ITO_CATEGORYPATH] LIKE '%Watch%' THEN 'Watch'
-          WHEN [ITO_CATEGORYPATH] LIKE '%OBA%' THEN 'OBA'
-          WHEN [ITO_CATEGORYPATH] LIKE '%Accessories%' THEN 'Accessories'
-          WHEN [ITO_CATEGORYPATH] LIKE '%Spare%' THEN 'Spare'
-          ELSE ''
-       END AS [type]
-FROM [MWD_DB].[dbo].[ECORESCATEGORY]
+WITH HIERARCHY  AS
+(
+ SELECT ROW_NUMBER() OVER(ORDER BY [ITO_CATEGORYPATH]) AS [id]
+     ,[NAME] AS 'name'
+     ,[ITO_PARENTCATEGORYPATH] AS 'parent'
+     ,[ITO_CATEGORYPATH] AS 'path'
+     ,CASE
+         WHEN [ITO_CATEGORYPATH] LIKE 'Mouawad Inventory\Merchandise\Jewelry%' THEN 'Jewelry'
+         WHEN [ITO_CATEGORYPATH] LIKE 'Mouawad Inventory\Merchandise\Stone%' THEN 'Stone'
+         WHEN [ITO_CATEGORYPATH] LIKE 'Mouawad Inventory\Merchandise\Watch%' THEN 'Watch'
+         WHEN [ITO_CATEGORYPATH] LIKE 'Mouawad Inventory\Merchandise\OBA%' THEN 'OBA'
+         WHEN [ITO_CATEGORYPATH] LIKE 'Mouawad Inventory\Merchandise\Accessories%' THEN 'Accessories'
+         WHEN [ITO_CATEGORYPATH] LIKE 'Mouawad Inventory\Merchandise\Spare%' THEN 'Spare'
+         ELSE ''
+      END AS [type]
+ FROM [MWD_DB].[dbo].[ECORESCATEGORY]
+)
+
+SELECT *
+FROM HIERARCHY
 ORDER BY path
