3b6be85fd431d7133653415acdb40a02829d0b7b
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2016-08-17 16:51:33 +0700

    delete file

--- data-migration/src/lib/utils/node.js
+++
@@ -1,9 +0,0 @@
-export default class Node {
-  constructor(hierarchy) {
-    this.id = hierarchy.split('\\').length
-    const [path, parent, name, ...rest] = hierarchy.match(/(.+\\)*([^\\]+)$/)
-    this.path = path
-    this.parent = parent || ''
-    this.name = name
-  }
-}
