34b3ee05a3526f197546b42cc3efb54854135e79
Nopparit Paksee <nopparit.paksee@itorama.com>
2016-08-22 15:10:30 +0700

    new query for product relateditem

--- web/code/plugins/http/src/containers/products/productreletedetail.js
+++ web/code/plugins/http/src/containers/products/productreletedetail.js
@@ -56,7 +56,11 @@ class productreletedetail extends Component {
 
         const  Detail  = this.props.productdetail;
         if(Detail.type != 'STO'){
-          this.props.getProductRelete(Detail.subType,1,productId);
+          const logindata = sessionStorage.logindata ? JSON.parse(sessionStorage.logindata) : null;
+          const currency = logindata.currency;
+         this.props.getProductRelete(Detail.subType,1,productId,Detail.dominant,currency,Detail.price[currency])
+        }
+
         }
         this.setState({
           productdetailLoading: false
@@ -139,7 +143,10 @@ class productreletedetail extends Component {
       const productlist = this.props.productlist;
       this.props.getProductDetail(productId).then(()=>{
         const  Detail  = this.props.productdetail;
-        this.props.getProductRelete(Detail.subType,1,productId);
+        const logindata = sessionStorage.logindata ? JSON.parse(sessionStorage.logindata) : null;
+        const currency = logindata.currency;
+        this.props.getProductRelete(Detail.subType,1,productId,Detail.dominant,currency,Detail.price[currency])
+
         this.setState({
           productdetailLoading: false
         });
@@ -467,8 +474,11 @@ class productreletedetail extends Component {
        const { totalpage,products,page } = this.props.productrelete;
        //const reletepage = this.props.productreletepage;
        const productId = this.props.params.id;
-       const { type,collection,subType } = this.props.productdetail;
+       const { type,collection,subType,price,dominant } = this.props.productdetail;
        const { fields: { reletepage },handleSubmit} = this.props;
+       const logindata = sessionStorage.logindata ? JSON.parse(sessionStorage.logindata) : null;
+       const currency = logindata.currency;
+       console.log("dominant",dominant);
        if(!products){
          return(
            <div><center><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><Loading type="spin" color="#202020" width="10%"/></center></div>
@@ -491,7 +501,7 @@ class productreletedetail extends Component {
                 items={totalpage}
                 maxButtons={3}
                 activePage={reletepage.defaultValue}
-                onSelect={(eventKey) => { this.props.getProductRelete(subType,eventKey,productId); }} />
+                onSelect={(eventKey) => { this.props.getProductRelete(subType,eventKey,productId,dominant,currency,price[currency]); }} />
                 <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12 nopadding">
                   <span>Page</span>
                   <form onSubmit={handleSubmit(this.handleGo)} >
@@ -514,9 +524,11 @@ class productreletedetail extends Component {
       const productId = this.props.params.id;
       const { totalpage} = this.props.productrelete;
       const getPage = parseInt(data.reletepage);
-      const { collection,subType } = this.props.productdetail;
+      const { collection,subType,price,dominant } = this.props.productdetail;
       if((getPage <= totalpage) && (getPage != 0)){
-      this.props.getProductRelete(subType,getPage,productId);
+        const logindata = sessionStorage.logindata ? JSON.parse(sessionStorage.logindata) : null;
+        const currency = logindata.currency;
+        this.props.getProductRelete(subType,getPage,productId,dominant,currency,price[currency]);
       }
     }
 
