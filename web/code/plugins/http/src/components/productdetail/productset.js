640b4c2baa213932341bd2710b05dd91433d26d0
Nopparit Paksee <nopparit.paksee@itorama.com>
2016-08-23 15:30:04 +0700

    fix error apiurl product set-referrence and imagepath set-refference

--- web/code/plugins/http/src/components/productdetail/productset.js
+++ web/code/plugins/http/src/components/productdetail/productset.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router';
 const pructdetailurl = '/productreletedetail/';
 
 const productSet =  (props) =>{
-
+    console.log(props.productset.products);
     return (
          <div>
+
          {props.productset.products.map(function(data, index){
             return (
               <div key={data.id} className="col-md-3 col-sm-3 bd-img nopadding">
-                <Link to={{pathname: `${pructdetailurl}${data.id}`}}><img src={data.image.length > 0 ? data.image[0].thumbnail :'/images/blank.gif' }  responsive width={120} height={120}/></Link>
+                <Link to={{pathname: `${pructdetailurl}${data.id}`}}><img src={data.image ? data.image.original :'/images/blank.gif' }  responsive width={120} height={120}/></Link>
               </div>
            )
            })}
