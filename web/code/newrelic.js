dccb37a22955ba0d6f4e786d1508ffb78b1dfab3
Jittawe Tandhanand <jittawe@itorama.com>
2016-09-07 12:37:57 +0700

    ignore newrelic.js

--- web/code/newrelic.js
+++ web/code/newrelic.js
@@ -7,20 +7,10 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-  /**
-   * Array of application names.
-   */
-  app_name: ['mol'],
-  /**
-   * Your New Relic license key.
-   */
-  license_key: 'b5c40e1121e01da2bcadef91d19454e3feb4f35c',
-  logging: {
-    /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
-     */
-    level: 'info'
-  }
+   app_name: ['mol'],
+   license_key: 'b5c40e1121e01da2bcadef91d19454e3feb4f35c',
+   logging: {
+       level: 'info'
+   },
+   agent_enabled: false
 }
