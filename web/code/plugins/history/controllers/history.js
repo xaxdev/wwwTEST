f9e11967ae398f6e876dc43228a0268925875e0e
Kasemsun J <kasemsun.jatnawong@itorama.com>
2016-09-07 19:28:03 +0700

    extract user from token

--- web/code/plugins/history/controllers/history.js
+++ web/code/plugins/history/controllers/history.js
@@ -1,12 +1,29 @@
+const Boom = require('boom');
+
 export default {
     auth: {
         strategy: 'authentication'
     },
     handler: (request, reply) => {
 
-        (async _ => {
+        console.log(JSON.stringify(request.auth, null, 4));
 
-            reply('Get History!');
-        })();
+        try {
+            (async _ => {
+
+                var db = request.server.plugins['hapi-mongodb'].db;
+                var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
+
+                db.collection('User').findOne({  "_id" : new ObjectID('577ddaec73b1eb082aab4bfc') }, function(err, result) {
+
+                    if (err) return reply(Boom.internal('Internal MongoDB error', err));
+
+                    reply(result);
+                });
+            })();
+        } catch (e) {
+
+            reply(Boom.internal(e));
+        }
     }
 }
