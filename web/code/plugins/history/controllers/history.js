1442febba6ce3bede21615a1cd169b988ce87e03
Kasemsun J <iconv@hotmail.com>
2016-09-11 22:59:36 +0700

    display history with pagination functional

--- web/code/plugins/history/controllers/history.js
+++ web/code/plugins/history/controllers/history.js
@@ -1,4 +1,5 @@
 const Boom = require('boom')
+const _ = require('lodash')
 
 export default {
     auth: {
@@ -6,11 +7,44 @@ export default {
     },
     handler: (request, reply) => {
 
-        (async _ => {
+        (async () => {
+
+            const display = request.params.display || "ACTIVE"
+            const page = request.params.page || 1
+            const size = 8
+            let fCondition = { "userId": request.auth.credentials.id }
+
+            if (display != undefined) {
+                switch(display.toUpperCase()){
+                    case "ALL":
+                        fCondition = fCondition
+                        break;
+                    case "ACTIVE":
+                        fCondition = _.assign({ "displayStatus": true }, fCondition)
+                        break;
+                    case "DEACTIVE":
+                        fCondition = _.assign({ "displayStatus": false }, fCondition)
+                        break;
+                    default:
+                        fCondition = _.assign({ "displayStatus": true }, fCondition)
+                }
+            }
 
             try {
                 const db = request.server.plugins['hapi-mongodb'].db
-                reply(await db.collection('History').find({ "userId": request.auth.credentials.id }).toArray())
+                let data = await
+                    db.collection('History')
+                    .find(fCondition)
+                    .sort({ "lookUpDate": -1 })
+                    .toArray()
+
+                reply({
+                    "status": true,
+                    "page": parseInt(page),
+                    "total_items": data.length,
+                    "total_pages": Math.ceil(data.length / size),
+                    "items": data.slice((page - 1) * size, page * size)
+                })
             } catch (e) {
 
                 reply(Boom.badImplementation('', e))
