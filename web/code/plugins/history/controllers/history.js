086cf278844a463a4b874768ac8fe4850b6fccb4
Kasemsun J <iconv@hotmail.com>
2016-09-18 14:10:16 +0700

    get item with user-privilege qualification

--- web/code/plugins/history/controllers/history.js
+++ web/code/plugins/history/controllers/history.js
@@ -9,9 +9,11 @@ export default {
 
         (async () => {
 
+            const history = request.history;
             const display = request.params.display || "ACTIVE"
             const page = request.params.page || 1
             const size = 8
+
             let fCondition = { "userId": request.auth.credentials.id }
 
             if (display != undefined) {
@@ -32,23 +34,66 @@ export default {
 
             try {
                 const db = request.server.plugins['hapi-mongodb'].db
-                let data = await
-                    db.collection('History')
-                    .find(fCondition)
-                    .sort({ "lookUpDate": -1 })
-                    .toArray()
-                let items = await
-                    db.collection('Items')
-                    .find({ "id": { $in: _.map(data, "itemId") } })
-                    .toArray()
+                const user = await history.getUserById(request, reply, request.auth.credentials.id)
 
-                reply({
-                    "status": true,
-                    "page": parseInt(page),
-                    "total_items": items.length,
-                    "total_pages": Math.ceil(items.length / size),
-                    "items": items.slice((page - 1) * size, page * size)
+                db.collection('History')
+                .find(fCondition)
+                .sort({ "lookUpDate": -1 })
+                .toArray()
+                .then((data) => {
+
+                    if (data) {
+                        let itemsCondition = { "id": { $in: _.map(data, "itemId") }}
+                        if (user.permission.onhandLocation.places.length != 0) {
+                            itemsCondition = _.assign({ "site": { $in: user.permission.onhandLocation.places }}, itemsCondition)
+                        }
+                        if (user.permission.onhandWarehouse.places.length != 0) {
+                            itemsCondition = _.assign({ "warehouse": { $in: user.permission.onhandWarehouse.places }}, itemsCondition)
+                        }
+
+                        return db.collection('Items')
+                        .find(itemsCondition)
+                        .toArray()
+                        .then((data) => {
+
+                            if (!_.isNull(data)) {
+                                data.forEach((item) => {
+
+                                    item.actualCost = _.hasIn(item.actualCost, user.currency) ? _.result(item.actualCost, user.currency) : 0
+                                    item.updatedCost = _.hasIn(item.updatedCost, user.currency) ? _.result(item.updatedCost, user.currency) : 0
+                                    item.price = _.hasIn(item.price, user.currency) ? _.result(item.price, user.currency) : 0
+
+                                    switch (user.permission.price.toUpperCase()) {
+                                        case "PUBLIC":
+                                            delete item.actualCost
+                                            delete item.updatedCost
+                                            break;
+                                        case "UPDATED":
+                                            delete item.actualCost
+                                            break;
+                                    }
+                                })
+
+                                return data;
+                            }
+
+                            return reply(Boom.badRequest("Invalid item."))
+                        })
+                    }
+
+                    return reply(Boom.badRequest("Invalid item."))
                 })
+                .then((data) => {
+
+                    return reply({
+                        "status": true,
+                        "page": parseInt(page),
+                        "total_items": data.length,
+                        "total_pages": Math.ceil(data.length / size),
+                        "items": data.slice((page - 1) * size, page * size)
+                    })
+                })
+
             } catch (e) {
 
                 reply(Boom.badImplementation('', e))
