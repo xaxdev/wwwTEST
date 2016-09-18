1e6aac5098c68822cb1f25320dfedc4c81c5b91e
Kasemsun J <iconv@hotmail.com>
2016-09-18 14:10:16 +0700

    get item with user-privilege qualification

--- web/code/plugins/mobile/items/controllers/searchreference.js
+++ web/code/plugins/mobile/items/controllers/searchreference.js
@@ -9,13 +9,23 @@ module.exports = {
     auth: {
         strategy: 'authentication'
     },
-    handler: (request, reply) => {
+    handler: async (request, reply) => {
 
         const db = request.server.plugins['hapi-mongodb'].db
-        const reference = request.params.reference;
-        const history = request.history;
+        const reference = request.params.reference
+        const history = request.history
+        const user = await history.getUserById(request, reply, request.auth.credentials.id)
+        let fCondition = { "reference": reference }
 
-        db.collection('Items').findOne({ "reference": reference })
+        if (user.permission.onhandLocation.places.length != 0) {
+            fCondition = _.assign({ "site": { $in: user.permission.onhandLocation.places }}, fCondition)
+        }
+
+        if (user.permission.onhandWarehouse.places.length != 0) {
+            fCondition = _.assign({ "warehouse": { $in: user.permission.onhandWarehouse.places }}, fCondition)
+        }
+
+        db.collection('Items').findOne(fCondition)
         .then(async (item) => {
 
             if (!_.isNull(item)) {
@@ -27,6 +37,20 @@ module.exports = {
         .then((item) => {
 
             if (!_.isNull(item)) {
+                item.actualCost = _.hasIn(item.actualCost, user.currency) ? _.result(item.actualCost, user.currency) : 0
+                item.updatedCost = _.hasIn(item.updatedCost, user.currency) ? _.result(item.updatedCost, user.currency) : 0
+                item.price = _.hasIn(item.price, user.currency) ? _.result(item.price, user.currency) : 0
+
+                switch (user.permission.price.toUpperCase()) {
+                    case "PUBLIC":
+                        delete item.actualCost
+                        delete item.updatedCost
+                        break;
+                    case "UPDATED":
+                        delete item.actualCost
+                        break;
+                }
+
                 return reply(JSON.stringify(item, null, 4))
             }
 
