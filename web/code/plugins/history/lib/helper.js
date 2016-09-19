beaa5f1dff630785f33e14f647d30c0ae36dfcb3
Kasemsun J <kasemsun.jantawong@itorama.com>
2016-09-19 16:22:03 +0700

    modify history helper

--- web/code/plugins/history/lib/helper.js
+++ web/code/plugins/history/lib/helper.js
@@ -1,6 +1,9 @@
 const Boom = require('boom');
 const Promise = require('bluebird');
 const _ = require('lodash');
+const internals = {
+    filters: []
+};
 
 export default {
     save: (request, reply, item) => {
@@ -55,5 +58,141 @@ export default {
                 return Boom.badImplementation('', e)
             }
         })(request, reply, userId);
+    },
+
+    getItemDataById: (request, reply, itemId) => {
+
+        const elastic = request.server.plugins.elastic.client;
+
+        internals.query = JSON.parse(
+            `{
+                "query":
+                {
+                    "constant_score" : {
+                        "query" : {
+                            "bool" : {
+                                "should" : [{
+                                    "match" : {
+                                        "id" : "5637209721"
+                                    }
+                                }, {
+                                    "match" : {
+                                        "id" : "5637234633"
+                                    }
+                                }, {
+                                    "match" : {
+                                        "id" : "5637222093"
+                                    }
+                                }, {
+                                    "match" : {
+                                        "id" : "5637870664"
+                                    }
+                                }, {
+                                    "match" : {
+                                        "id" : "5637209718"
+                                    }
+                                }, {
+                                    "match" : {
+                                        "id" : "5637230194"
+                                    }
+                                }, {
+                                    "match" : {
+                                        "id" : "5637249602"
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        }`);
+
+        const getProductDetail =  elastic
+        .search({
+            index: 'mol',
+            type: 'items',
+            body: internals.query
+        });
+
+        const getSetreference = getProductDetail.then((response) => {
+
+            const productResult = response.hits.hits.map((element) => element._source);
+            const query = JSON.parse(
+                `{
+                    "query":{
+                        "constant_score": {
+                            "filter": {
+                                "bool": {
+                                    "must": [
+                                        {
+                                            "match": {
+                                                "reference": "${productResult.setReference}"
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                }`);
+
+                return elastic.search({
+                    index: 'mol',
+                    type: 'setitems',
+                    body: query
+                });
+            });
+
+        Promise.all([getProductDetail, getSetreference]).spread((productDetail, setReference) => {
+
+            const productResult = productDetail.hits.hits.map((element) => element._source);
+            if(typeof productResult.gemstones !== 'undefined'){
+                const images = productResult.gemstones.reduce((accumulator, gemstone) => {
+                    let data = [];
+
+                    if (gemstone && gemstone.certificate && gemstone.certificate.images) {
+                        data = gemstone.certificate.images;
+                    }
+
+                    return accumulator.concat(data);
+                }, []);
+
+                productResult.gallery = productResult.gallery.concat(images);
+            }
+
+            const [setReferenceData] = setReference.hits.hits.map((element) => element._source);
+            if(typeof setReferenceData === 'undefined'){
+                productResult.setReferenceData = '';
+
+            } else {
+                let len = setReferenceData.items.length;
+
+                let productdata = [];
+                for (let i = 0; i < len; i++) {
+                    if(productResult.id !== setReferenceData.items[i].id){
+                        productdata.push({
+                            id: setReferenceData.items[i].id,
+                            image:setReferenceData.items[i].image
+                        });
+                    }
+                }
+                const responseSetData = {
+                    totalprice:setReferenceData.totalPrice,
+                    setimage:setReferenceData.image.original,
+                    products:productdata
+                }
+
+                productResult.setReferenceData = responseSetData;
+            }
+
+            // console.log(productResult);
+
+            elastic.close();
+            return JSON.stringify(productResult, null, 4);
+        })
+        // .catch(function(error) {
+        //     elastic.close();
+        //     return Boom.badImplementation(err);
+        // });
     }
 }
