22e1bfcbe99dac01d6ba017abe4ce6311666aed9
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2016-10-05 16:09:01 +0700

    Fixed defect2.0 pagination number, remove dominant stone, last page when change page view, field origin.

--- web/code/plugins/http/src/containers/searchresults/search_reslts.js
+++ web/code/plugins/http/src/containers/searchresults/search_reslts.js
@@ -196,25 +196,6 @@ class SearchResult extends Component {
   }
   componentDidMount() {
     // console.log('sortingBy->',this.refs.sortingBy);
-    var numbers = document.querySelectorAll('input[type="number"]');
-
-    for (var i in numbers) {
-      if (numbers.hasOwnProperty(i)) {
-        numbers[i].onkeydown = function(e) {
-            if(!((e.keyCode > 95 && e.keyCode < 106)
-              || (e.keyCode > 47 && e.keyCode < 58)
-              || e.keyCode == 8
-              || e.keyCode == 37
-              || e.keyCode == 39
-              || e.keyCode == 46
-              || e.keyCode == 110
-              || e.keyCode == 190
-              )) {
-                return false;
-            }
-        }
-      }
-    }
 
     let that = this;
     if(this.refs.sortingBy != undefined){
