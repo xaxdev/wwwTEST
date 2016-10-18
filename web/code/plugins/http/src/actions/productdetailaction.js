3c0c188cda1992a200f50a6ebc8d4c4597fd106c
Nopparit Paksee <nopparit.paksee@itorama.com>
2016-10-18 10:16:51 +0700

    hot fix productdetail token

--- web/code/plugins/http/src/actions/productdetailaction.js
+++ web/code/plugins/http/src/actions/productdetailaction.js
@@ -5,10 +5,15 @@ import { FETCH_PRODUCTDETAIL,FETCH_PRODUCTRELETED,ROOT_URL,FETCH_SETREFERENCE} f
 // const fakeApiurl = 'http://localhost:4500/jewelry/';
 
 export function getProductDetail(productId,productlist){
-
+  const token = sessionStorage.token;
   return {
           type: FETCH_PRODUCTDETAIL,
     		  promise: fetch(`${ROOT_URL}api/items/${productId}`,{
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': token
+          },
           method: 'GET'
         }),
         productid:productId,
@@ -17,9 +22,15 @@ export function getProductDetail(productId,productlist){
 }
 
 export function getProductRelete(collection,page,productId,dominant,currency,price){
+  const token = sessionStorage.token;
   return {
       type: FETCH_PRODUCTRELETED,
       promise: fetch( `${ROOT_URL}api/items/relateditems/${collection}/${page}/${productId}/${dominant}/${currency}/${price}`,{
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': token
+        },
         method:'GET'
       }),
       page:page
@@ -27,9 +38,15 @@ export function getProductRelete(collection,page,productId,dominant,currency,pri
 }
 
 export function getSetreference(setreference,productId){
+  const token = sessionStorage.token;
   return {
       type: FETCH_SETREFERENCE,
       promise: fetch( `${ROOT_URL}api/items/setreference/${encodeURIComponent(setreference)}/${productId}`,{
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': token
+        },
         method:'GET'
       })
   }
