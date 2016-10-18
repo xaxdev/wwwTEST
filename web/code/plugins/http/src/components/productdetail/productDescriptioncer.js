35501b0823dbc8820a528e6a45ee2e908bcd59de
Nopparit Paksee <nopparit.paksee@itorama.com>
2016-10-18 14:17:46 +0700

    change label and field site to company

--- web/code/plugins/http/src/components/productdetail/productDescriptioncer.js
+++ web/code/plugins/http/src/components/productdetail/productDescriptioncer.js
@@ -23,8 +23,8 @@ const Detail = (props) =>{
           <div className="col-md-8 col-sm-8 text-wrap">{convertBlanktodash(props.name)}</div>
         </div>
         <div className="col-md-12 col-sm-12 nopadding">
-          <div className="col-md-4 col-sm-4 nopadding font-b">Site</div>
-          <div className="col-md-8 col-sm-8">{convertBlanktodash(props.siteName)}</div>
+          <div className="col-md-4 col-sm-4 nopadding font-b">Company</div>
+          <div className="col-md-8 col-sm-8">{convertBlanktodash(props.companyName)}</div>
         </div>
         <div className="col-md-12 col-sm-12 nopadding">
           <div className="col-md-4 col-sm-4 nopadding font-b">Warehouse</div>
