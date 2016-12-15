10158b3b4e72db4652f6097d80bcc6c6bfbe7332
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2016-12-15 14:18:32 +0700

    Create action for news.

--- web/code/plugins/http/src/containers/whatnewnotification/whatnewnotification.js
+++ web/code/plugins/http/src/containers/whatnewnotification/whatnewnotification.js
@@ -1,58 +1,85 @@
 import React, { Component, PropTypes }from 'react';
 import { reduxForm, reset } from 'redux-form';
+import { Accordion, AccordionItem } from 'react-sanfona';
 
 class WhatNewNotification extends Component {
 
     render() {
+            const Title = 'What\'s news';
+            const roundData = [1];
+            const msgData = [
+                                {
+                                    'id': 1,
+                                    'round': 'July 2016',
+                                    'datas': [
+                                            {
+                                                'type':'new',
+                                                'title': 'Add Accessory Catalog',
+                                                'msg': `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> Vestibulum diam odio, eleifend a vehicula non, feugiat ac tellus.
+                                                Pellentesque velit orci, hendrerit a risus non, elementum vehicula justo. Sed maximus arcu eu nisl pretium facilisis.
+                                                <div><img src="/images/what-new.jpg" className="maring-t15" /></div>`,
+                                                'img': '/images/what-new.jpg'
+                                            },
+                                            {
+                                                'type': 'update',
+                                                'title': 'Customize Notification',
+                                                'msg': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum diam odio, eleifend a vehicula non, feugiat ac tellus.
+                                                Pellentesque velit orci, hendrerit a risus non, elementum vehicula justo. Sed maximus arcu eu nisl pretium facilisis.`,
+                                                'img': ''
+                                            },
+                                            {
+                                                'type':'bug',
+                                                'title': 'Synchronizing Data with AX',
+                                                'msg': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum diam odio, eleifend a vehicula non, feugiat ac tellus.
+                                                Pellentesque velit orci, hendrerit a risus non, elementum vehicula justo. Sed maximus arcu eu nisl pretium facilisis.`,
+                                                'img': ''
+                                            }
+                                        ]
+                                }
 
+                            ];
             return(
                 <div className="col-sm-12">
                     <div className="whatnew-bg">
-                      what s news
+                      {Title}
                     </div>
                     <div className="accordion">
-                        <div className="accordion-item">
-                          <div className="accordion-title">
-                            suptember 2016
-                            <div className="accordion-open"></div>
-                          </div>
-                          <div className="whatnew-detail">
-                              <div className="accordion-detail">
-                                 <div className="accordion-detail-title">Add Accessory Catalog</div>
-                                 <div className="new float-r">New</div>
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum diam odio, eleifend a vehicula non, feugiat ac tellus.
-                                 Pellentesque velit orci, hendrerit a risus non, elementum vehicula justo. Sed maximus arcu eu nisl pretium facilisis.</p>
-                                 <img src="/images/what-new.jpg" className="maring-t15" />
-                              </div>
-                              <div className="accordion-detail">
-                                 <div className="accordion-detail-title">Customize Notification</div>
-                                 <div className="update float-r">Update</div>
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum diam odio, eleifend a vehicula non, feugiat ac tellus.
-                                 Pellentesque velit orci, hendrerit a risus non, elementum vehicula justo. Sed maximus arcu eu nisl pretium facilisis.</p>
-                              </div>
-                              <div className="accordion-detail">
-                                 <div className="accordion-detail-title">Synchronizing Data with AX</div>
-                                 <div className="bug float-r">Bug-Fix</div>
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum diam odio, eleifend a vehicula non, feugiat ac tellus.
-                                 Pellentesque velit orci, hendrerit a risus non, elementum vehicula justo. Sed maximus arcu eu nisl pretium facilisis.</p>
-                              </div>
-                            </div>
-                        </div>
+                        <Accordion activeItems={roundData.length}>
+                            {roundData.map((item, index) => {
+                                console.log(item);
+                                console.log(msgData);
+                                let loop = item-1;
+                                return (
+                                    <AccordionItem title={msgData[loop].round} slug={item} key={index} className="accordion-title">
+                                        <div className="whatnew-detail">
+                                            {msgData[loop].datas.map((data) => {
 
-                        <div className="accordion-item">
-                          <div className="accordion-title">
-                            August 2016
-                            <div className="accordion-close"></div>
-                          </div>
-                        </div>
-
-                        <div className="accordion-item">
-                          <div className="accordion-title">
-                            July 2016
-                            <div className="accordion-close"></div>
-                          </div>
-                        </div>
-
+                                                let typeNews = '';
+                                                switch (data.type) {
+                                                    case 'update':
+                                                        typeNews = 'Update';
+                                                        break;
+                                                    case 'bug':
+                                                        typeNews = 'Bug-Fix';
+                                                        break;
+                                                    default:
+                                                        typeNews = 'New';
+                                                        break;
+                                                }
+                                                console.log(data.msg);
+                                                return (
+                                                    <div className="accordion-detail">
+                                                        <div className="accordion-detail-title">{data.title}</div>
+                                                        <div className={`${data.type} float-r`}>{typeNews}</div>
+                                                        <div dangerouslySetInnerHTML={{__html: data.msg}}></div>
+                                                    </div>
+                                                );
+                                            })}
+                                        </div>
+                                    </AccordionItem>
+                                );
+                            })}
+                        </Accordion>
                     </div>
                 </div>
             );
