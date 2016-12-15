029f60169a47159b9cefcd16523ea7d4853495e5
Sangrawee Deethubthim <sangrawee.deethubthim@itorama.com>
2016-12-15 16:16:20 +0700

    modify what new css

--- web/code/plugins/http/src/containers/whatnewnotification/whatnewnotification.js
+++ web/code/plugins/http/src/containers/whatnewnotification/whatnewnotification.js
@@ -10,30 +10,89 @@ class WhatNewNotification extends Component {
             const msgData = [
                                 {
                                     'id': 1,
-                                    'round': 'July 2016',
+                                    'round': 'December 2016',
                                     'datas': [
                                             {
                                                 'type':'new',
-                                                'title': 'Add Accessory Catalog',
-                                                'msg': `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> Vestibulum diam odio, eleifend a vehicula non, feugiat ac tellus.
-                                                Pellentesque velit orci, hendrerit a risus non, elementum vehicula justo. Sed maximus arcu eu nisl pretium facilisis.
-                                                <div><img src="/images/what-new.jpg" className="maring-t15" /></div>`,
-                                                'img': '/images/what-new.jpg'
+                                                'title': 'User manual of MOL',
+                                                'msg': `<p>Download User Manual</p>`
                                             },
                                             {
-                                                'type': 'update',
-                                                'title': 'Customize Notification',
-                                                'msg': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum diam odio, eleifend a vehicula non, feugiat ac tellus.
-                                                Pellentesque velit orci, hendrerit a risus non, elementum vehicula justo. Sed maximus arcu eu nisl pretium facilisis.`,
-                                                'img': ''
+                                                'type': 'new',
+                                                'title': '1.	Inventory Report',
+                                                'msg': `<p>Under the header search, all the filters applied to all product categories</p>
+                                                <img src="/images/mol-1.jpg"/>
+                                                <p style="padding:20px 0 0 0;">Or filtering any criteria in each category under the Advance Search</p>
+                                                <img src="/images/mol-1-1.jpg" />`
                                             },
                                             {
-                                                'type':'bug',
-                                                'title': 'Synchronizing Data with AX',
-                                                'msg': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum diam odio, eleifend a vehicula non, feugiat ac tellus.
-                                                Pellentesque velit orci, hendrerit a risus non, elementum vehicula justo. Sed maximus arcu eu nisl pretium facilisis.`,
-                                                'img': ''
+                                                'type':'new',
+                                                'title': '2.	6 Product Categories',
+                                                'msg': `<p>There are 6 product categories such Jewelry, Watch, Stone, Accessory, Object of Art and Spare Part.  Filtering in each category by selected the category and fill the criteria.</p>
+                                                <img src="/images/mol-2.jpg" />`
+                                            },
+                                            {
+                                                'type':'new',
+                                                'title': '3.	Search any items from Excel file',
+                                                'msg': `<p>The simple steps to search any items from your Excel file:
+                                                <div style="padding:0px 20px 10px 20px;">
+                                                   <div>1.	Download Excel template</div>
+                                                   <div>2.	Fill the item reference in the Excel file</div>
+                                                   <div>3.	Upload the file in MOL by clicking “Browse” button and select the file</div>
+                                                 </div>
+                                                 </p>
+                                                 <img src="/images/mol-3.jpg"/>`
+                                            },
+                                            {
+                                                'type':'new',
+                                                'title': '4.	Search Result',
+                                                'msg': `<p>There are the friendly functions under the Search Result page:</p>
+                                                <div style="padding:0px 20px 10px 20px;">
+                                                   <p>•	List and Grid style </p>
+                                                   <p>•	Quick View and Product detail</p>
+                                                   <p>•	Number of items in a page</p>
+                                                   <p>•	My catalog</p>
+                                                   <p>•	Sorting and Export the data</p>
+                                                 </div>
+                                                 <img src="/images/mol-4.jpg"/>`
+                                            },
+                                            {
+                                                'type':'new',
+                                                'title': '5.	Export data to Excel file, and send to your e-mail',
+                                                'msg': `<p style="padding:0px 0px 15px 0px;">Click at “Excel” icon  <img src="/images/mol-5-1.jpg"/> to export data to Excel file, that the system will send the download link to your e-mail. </p>
+                                                 <img src="/images/mol-5.jpg"/>`
+                                            },
+                                            {
+                                                'type':'new',
+                                                'title': '6.	Export data to PDF file',
+                                                'msg': `<p style="padding:0px 0px 15px 0px;">Click at “Print” icon  <img src="/images/mol-6-1.jpg" />  to print to PDF file, that the system will send the download link to your e-mail.</p>
+                                                 <img src="/images/mol-6.jpg" />`
+                                            },
+                                            {
+                                                'type':'new',
+                                                'title': '7.	Change Password',
+                                                'msg': `<p>Changing your password, by clicking at your username and click Change Password menu.</p>
+                                                 <div style="margin-top:15px; text-align:center;"><img src="/images/mol-7.jpg" /></div>
+                                                 <div style="margin-top:15px; text-align:center;"><img src="/images/mol-7-1.jpg" /></div>`
+                                            },
+                                            {
+                                                'type':'new',
+                                                'title': '8.	My catalog',
+                                                'msg': `<p>Manage and view your catalog My Catalog menu.</p>
+                                                 <div style="margin-top:15px; text-align:center;"><img src="/images/mol-8.jpg"/></div>
+                                                <div style="padding-top:15px;">Management tools</div>
+                                                 <div style="margin-top:15px; text-align:center;"><img src="/images/mol-8-1.jpg"/></div>
+                                                <div style="padding:15px 0;">How to add items to catalog</div>
+                                                <div styl="padding:15px 20px 0 20px;">
+                                                  <div>1.	User adds item to My Catalog at Search Result page and Product Detail page</div>
+                                                  <div>2.	Add an item to catalog: click “Catalog” icon <img src="/images/mol-8-2.jpg" /> at the selected items, and go to step 4</div>
+                                                  <div>3.	Add multiple items to catalog: click “Square” icon <img src="/images/mol-8-3.jpg" />   at the selected items, and click “Catalog” icon on bar <img src="/images/mol-8-4.jpg" />  , and go to step 4</div>
+                                                  <div style="padding-top:15px;"><img src="/images/mol-8-6.jpg"/></div>
+                                                  <div style="margin-top:15px;">4.	Select the existing catalog or create the new catalog</div>
+                                                  <div style="margin-top:15px; text-align:center;"><img src="/images/mol-8-7.jpg" /></div>
+                                                </div>`
                                             }
+
                                         ]
                                 }
 
@@ -44,6 +103,7 @@ class WhatNewNotification extends Component {
                       {Title}
                     </div>
                     <div className="accordion">
+                     <div className="accordion-item">
                         <Accordion activeItems={roundData.length}>
                             {roundData.map((item, index) => {
                                 console.log(item);
@@ -81,6 +141,7 @@ class WhatNewNotification extends Component {
                             })}
                         </Accordion>
                     </div>
+                  </div>
                 </div>
             );
     }
