47decd1697f3996e2398fef48d118de97333b87b
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2016-12-28 14:10:18 +0700

    Fixed css of pdf templace.

--- web/code/plugins/http/src/utils/genTemplatePdfMyCatalog.js
+++ web/code/plugins/http/src/utils/genTemplatePdfMyCatalog.js
@@ -22,7 +22,7 @@ export default function GenTemplateHtml(ROOT_URL, imagesReplace, dv){
     let fontbfc000  = `font-family: '${'open_sanssemibold'}';color: #000;margin: 0 0 10px;`;
     let productdetailh = 'height: 85px;overflow: hidden;word-wrap: break-word;margin: 0 0 10px;';
     let stylePrice = 'color: #ae8f3b; font-weight: bold;';
-    let thumbnaillgrid = 'margin: 0 auto; position: relative; width: 230px; height: 200px; overflow: hidden;';
+    let thumbnaillgrid = 'margin: 0 auto; height: 200px; overflow: hidden;';
     let thumbnaillgridimg = 'position: absolute; left: 50%; top: 50%; height: auto; width: auto; padding-bottom: 20px; transform: translate(-50%,-50%);';
 
     dvTotal = dvTotal.replace(/class="font-b fc-000"/g,'style="font-weight: bold; color: #000;"');
@@ -42,7 +42,7 @@ export default function GenTemplateHtml(ROOT_URL, imagesReplace, dv){
     dvGridview = dvGridview.replace(/class="product-detail-h"/g,`style="${productdetailh}"`);
     dvGridview = dvGridview.replace(/class="fc-ae8f3b font-b price "/g,`style="${stylePrice}"`);
     dvGridview = dvGridview.replace(/class="thumbnaillgrid"/g,`style="${thumbnaillgrid}"`);
-    dvGridview = dvGridview.replace(/<img/g,`<img style="${thumbnaillgridimg}" `);
+    dvGridview = dvGridview.replace(/<img/g,'<img style="width:120px" ');
     dvGridview = dvGridview.replace(/\/original\//g,'/thumbnail/');
 
     let htmlTemplate = '';
--- web/code/plugins/http/src/utils/genTemplatePdfSearchResult.js
+++ web/code/plugins/http/src/utils/genTemplatePdfSearchResult.js
@@ -23,8 +23,8 @@ export default function GenTemplateHtml(showGridView, showListView, ROOT_URL, im
     let fontbfc000  = `font-family: '${'open_sanssemibold'}';color: #000;margin: 0 0 10px;`;
     let productdetailh = 'height: 85px;overflow: hidden;word-wrap: break-word;margin: 0 0 10px;';
     let stylePrice = 'color: #ae8f3b; font-weight: bold;';
-    let thumbnaillgrid = 'margin: 0 auto; position: relative; width: 230px; height: 200px; overflow: hidden;';
-    let thumbnaillgridimg = 'position: absolute; left: 50%; top: 50%; height: auto; width: auto; padding-bottom: 20px; transform: translate(-50%,-50%);';
+    let thumbnaillgrid = 'margin: 0 auto; height: 200px; overflow: hidden;';
+    let thumbnaillgridimg = 'width:120px;';
 
     dvTotal1 = dvTotal1.replace(/class="font-b fc-000"/g,'style="font-weight: bold; color: #000;"');
     dvTotal1 = dvTotal1.replace(/class="padding-lf15"/g,'style="padding: 0 15px;"');
