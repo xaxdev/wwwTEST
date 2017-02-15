8f95b035f6262f5a47e182ba6f1686daf26e27fe
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-02-15 17:59:19 +0700

    Fixed search carat and qty for lot.

--- web/code/plugins/items/utils/getAllData.js
+++ web/code/plugins/items/utils/getAllData.js
@@ -18,34 +18,7 @@ module.exports = (response, sortDirections, sortBy, size, page, userCurrency, ke
   //   data = _.sortBy(data,sortBy,sortDirections);
   // }
 
-  exportData = data;
-  // console.log('data-->',data);
-  let maxPrice = 0;
-
   data.forEach(function(item){
-    allData.push({'id': item.id,'reference':item.reference});
-    if(item.price != undefined){
-      if(item.price[userCurrency] != undefined){
-        // console.log('item.reference-->',item.reference);
-        // console.log('item.price[userCurrency]-->',item.price[userCurrency]);
-        if(item.price[userCurrency] != 0){
-          maxPrice = Math.max(maxPrice, item.price[userCurrency]);
-        }else{
-          maxPrice = Math.max(maxPrice, 0);
-        }
-      }else{
-        item.price[userCurrency] = 0;
-        maxPrice = Math.max(maxPrice, 0);
-      }
-
-      // console.log('maxPrice-->',maxPrice);
-    }else{
-      if(maxPrice > 0){
-        maxPrice = maxPrice;
-      }else{
-        maxPrice = 0;
-      }
-    }
     // filter data from array lotnaumer
     if (item.type == 'STO') {
         let newLot = item.lotNumbers;
@@ -95,35 +68,60 @@ module.exports = (response, sortDirections, sortBy, size, page, userCurrency, ke
                 }
         }
         item.lotNumbers = newLot;
-        // console.log('lot-->',item.lotNumbers.length);
+        if (newLot.length == 0 ) {
+            data = data.filter((lot) => {
+                return lot.reference != item.reference
+            })
+        }
+    }
+  });
+
+  let maxPrice = 0;
+
+  data.forEach(function(item){
+    allData.push({'id': item.id,'reference':item.reference});
+
+    if(item.price != undefined){
+      if(item.price[userCurrency] != undefined){
+        // console.log('item.reference-->',item.reference);
+        // console.log('item.price[userCurrency]-->',item.price[userCurrency]);
+        if(item.price[userCurrency] != 0){
+          maxPrice = Math.max(maxPrice, item.price[userCurrency]);
+        }else{
+          maxPrice = Math.max(maxPrice, 0);
+        }
+      }else{
+        item.price[userCurrency] = 0;
+        maxPrice = Math.max(maxPrice, 0);
+      }
+
+      // console.log('maxPrice-->',maxPrice);
+    }else{
+      if(maxPrice > 0){
+        maxPrice = maxPrice;
+      }else{
+        maxPrice = 0;
+      }
     }
   });
 
   let minPrice = maxPrice;
   data.forEach(function(item){
-
-    if(item.price != undefined){
-      if(item.price[userCurrency] != undefined){
-        minPrice = Math.min(minPrice, item.price[userCurrency]);
+      if(item.price != undefined){
+        if(item.price[userCurrency] != undefined){
+          minPrice = Math.min(minPrice, item.price[userCurrency]);
+        }else{
+          minPrice = Math.min(minPrice, 0);
+        }
       }else{
-        minPrice = Math.min(minPrice, 0);
+        minPrice = 0;
       }
-    }else{
-      minPrice = 0;
-    }
   });
+  exportData = data;
 
-  // let pageData = data.max(size) ;
-  // console.log('page-->',page);
-  // console.log('size-->',size);
-  // console.log('data-->',data.length);
   let pageData = data.slice( (page - 1) * size, page * size );
   let sumPrice = 0;
   let sumCost = 0;
-
-  // console.log('pageData-->',pageData.length);
-  // console.log('maxPrice-->',maxPrice);
-  // console.log('minPrice-->',minPrice);
 
   if(pageData.length != 0){
     data.forEach(function(item){
