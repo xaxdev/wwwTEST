2f4a705e46ed713884bb311abc79e0a7faa06204
Unknown <guzzm@hotmail.com>
2017-02-20 17:41:37 +0700

    add movement bit

--- web/code/plugins/users/controllers/update.js
+++ web/code/plugins/users/controllers/update.js
@@ -6,117 +6,117 @@ const Bcrypt = require('bcrypt');
 const Promise = require('bluebird');
 
 module.exports = {
-  auth: {
-    strategy: 'authentication',
-    scope: ['admin']
-  },
-  validate: {
-    params: {
-      id: Joi.number().integer().positive().required()
-    }
-  },
-  pre: [{ method: (request, reply) => {
-    // console.log('pre-->');
-    const Users = request.collections.user;
+    auth: {
+        strategy: 'authentication',
+        scope: ['admin']
+    },
+    validate: {
+        params: {
+            id: Joi.number().integer().positive().required()
+        }
+    },
+    pre: [{ method: (request, reply) => {
+        // console.log('pre-->');
+        const Users = request.collections.user;
 
-    // Username can't be changed (after discussion with Goong on 29/02/2016)
-    delete request.payload.username;
+        // Username can't be changed (after discussion with Goong on 29/02/2016)
+        delete request.payload.username;
 
-    // Email must not be used already (after discussion with Goong on 29/02/2016)
-    if (request.payload.email) {
+        // Email must not be used already (after discussion with Goong on 29/02/2016)
+        if (request.payload.email) {
 
-      Users
-        .findOneByEmail(request.payload.email)
-        .then(function (user) {
+            Users
+            .findOneByEmail(request.payload.email)
+            .then(function (user) {
 
-          if (user && user.id !== request.params.id) {
-            return reply({
-              response: Boom.badRequest('this email is already in use.'),
-              status: false
-            });
-          }
+                if (user && user.id !== request.params.id) {
+                    return reply({
+                        response: Boom.badRequest('this email is already in use.'),
+                        status: false
+                    });
+                }
 
-          // encrypt password if exists
-          if (request.payload.password !== undefined) {
+                // encrypt password if exists
+                if (request.payload.password !== undefined) {
 
-            return internals
-            .encrypt(request.payload.password)
-            .then(function (hash) {
+                    return internals
+                    .encrypt(request.payload.password)
+                    .then(function (hash) {
 
-              request.payload.password = hash;
-              return reply({ status: true });
-            });
-          }
-          else {
+                        request.payload.password = hash;
+                        return reply({ status: true });
+                    });
+                }
+                else {
+                    return reply({ status: true });
+                }
+
+            })
+            .catch(function (err) {
+
+                return reply({
+                    response: Boom.badImplementation(err),
+                    status: false
+                });
+            })
+            .done();
+        }
+        else {
             return reply({ status: true });
-          }
+        }
 
+    }, assign: 'validation' }],
+    handler: (request, reply) => {
+        // console.log('handler-->');
+        const validation = request.pre.validation;
+
+        if (!validation.status) {
+            return reply(validation.response);
+        }
+
+        const Users = request.collections.user;
+        const Permissions = request.collections.permission;
+        const Authentication = request.collections.authentication;
+
+        // console.log(request.payload);
+
+        Users
+        .update({ id: request.params.id }, request.payload)
+        .then(function ([user, ...rest]) {
+
+            if(!user.status){
+                Authentication.destroy({user:user.email})
+                .exec(function (err){
+                    if (err) {
+                        return reply(Boom.badImplementation(err));
+                    }
+                });
+            }
+
+            return Permissions
+            .findOne({ id: user.permission })
+            // .populate('onhand')
+            .populate('onhandLocation')
+            .populate('onhandWarehouse')
+            .then(function (permission) {
+                // console.log('permission-->',permission);
+                user.permission = permission.toJSON();
+                return reply({ data: user });
+            });
         })
-        .catch(function (err) {
+        .catch((err) => {
 
-          return reply({
-            response: Boom.badImplementation(err),
-            status: false
-          });
+            return reply(Boom.badImplementation(err));
         })
         .done();
     }
-    else {
-      return reply({ status: true });
-    }
-
-  }, assign: 'validation' }],
-  handler: (request, reply) => {
-    // console.log('handler-->');
-    const validation = request.pre.validation;
-
-    if (!validation.status) {
-      return reply(validation.response);
-    }
-
-    const Users = request.collections.user;
-    const Permissions = request.collections.permission;
-    const Authentication = request.collections.authentication;
-
-    // console.log(request.payload);
-
-    Users
-      .update({ id: request.params.id }, request.payload)
-      .then(function ([user, ...rest]) {
-
-        if(!user.status){
-          Authentication.destroy({user:user.email})
-          .exec(function (err){
-            if (err) {
-              return reply(Boom.badImplementation(err));
-            }
-          });
-        }
-
-        return Permissions
-          .findOne({ id: user.permission })
-          // .populate('onhand')
-          .populate('onhandLocation')
-          .populate('onhandWarehouse')
-          .then(function (permission) {
-            // console.log('permission-->',permission);
-            user.permission = permission.toJSON();
-            return reply({ data: user });
-          });
-      })
-    .catch((err) => {
-
-      return reply(Boom.badImplementation(err));
-    })
-    .done();
-  }
 };
 
 const internals = {};
 
 internals.encrypt = (password) => {
 
-  const Encrypt = Promise.promisify(Bcrypt.hash);
+    const Encrypt = Promise.promisify(Bcrypt.hash);
 
-  return Encrypt(password, 10);
+    return Encrypt(password, 10);
 }
