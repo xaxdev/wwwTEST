76fbac8e2e0cd26487e46f86ff349c141e0a4416
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-02-27 18:25:51 +0700

    Fixed data goc show on page.

--- web/code/plugins/http/src/components/productdetail/productgoc.js
+++ web/code/plugins/http/src/components/productdetail/productgoc.js
@@ -24,14 +24,14 @@ const Goc =  ({list}) =>{
 
                if (data.status === 'GOC') {
                  transferfrom = `[${data.warehouse}] ${data.warehouseName}`
-                 transferto = `Customer Name (${data.firstName} ${data.middleName} ${data.lastName}) ${data.customerID}`
+                 transferto = `${data.firstName} ${data.middleName} ${data.lastName} (${data.customerID})`
                } else {
-                 transferfrom = `Customer Name (${data.firstName} ${data.middleName} ${data.lastName}) ${data.customerID}`
+                 transferfrom = `${data.firstName} ${data.middleName} ${data.lastName} (${data.customerID})`
                  transferto = `[${data.warehouse}] ${data.warehouseName}`
                }
                return (
                    <tr key={index}>
-                     <td title="Transfer Date" className="text-center">{!!data.timeTo ? convertDate(data.timeTo) : '-'}</td>
+                     <td title="Transfer Date" className="text-center">{!!data.date ? convertDate(data.date) : '-'}</td>
                      <td title="Transferred from" className="text-center">{transferfrom}</td>
                      <td title="Transferred to" className="text-center">{transferto}</td>
 
