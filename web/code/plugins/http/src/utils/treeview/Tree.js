2206df9e05086824d08d4621418ca66f5ddd9893
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-03-30 16:40:57 +0700

    Fixed hierarchy collapsed and display data checked or unchecked.

--- web/code/plugins/http/src/utils/treeview/Tree.js
+++ web/code/plugins/http/src/utils/treeview/Tree.js
@@ -15,6 +15,7 @@ var Tree = React.createClass({
     // var checked = evt.target.checked;
 	var checked = null;
 	var key = null;
+	let listParentNode = [];
 
 	var traverseNodes = function (node) {
 		// Check true at Node
@@ -32,6 +33,26 @@ var Tree = React.createClass({
 	var checkAllNodes = function (node) {
 	  node.checked = checked;
 	  if (node.children) { node.children.forEach(checkAllNodes); }
+	};
+
+	const traverseNodesUnChecked = function (node) {
+		if (key.indexOf(node.code) != -1) {
+			listParentNode.push(node.id);
+		}
+	  	if (node.children) {
+			node.children.forEach(traverseNodesUnChecked);
+	  	}
+	};
+
+	const setUncheckedParentNode = function (node) {
+		listParentNode.map((parent) => {
+			if (parent === node.id) {
+				node.checked = false;
+			}
+		})
+	  	if (node.children) {
+			node.children.forEach(setUncheckedParentNode);
+	  	}
 	};
 
 	if (!!evt[0]) {
@@ -58,24 +79,30 @@ var Tree = React.createClass({
 							? evt.key
 							: evt[0].code
 					: '';
-		// set primary node false
-		if(!checked){
-			this.state.data[0] = {...this.state.data[0], checked: false};
-		}
 
 		this.state.data.forEach(traverseNodes);
-
-		// set primary node true
-		const childrens = this.state.data[0].children.length;
-		let counts = 0;
-		this.state.data[0].children.map((val) => {
-			if (val.checked) {
-				counts++;
-				if (counts == childrens) {
-					this.state.data[0] = {...this.state.data[0], checked: true};
-				}
+		// set primary node false
+		if(!checked){
+			// this.state.data[0] = {...this.state.data[0], checked: false};
+			this.state.data[0].checked = false;
+			this.state.data.forEach(traverseNodesUnChecked);
+			if (listParentNode.length != 1) {
+				this.state.data.forEach(setUncheckedParentNode);
 			}
-		})
+		}else{
+			// set primary node true
+			const childrens = this.state.data[0].children.length;
+			let counts = 0;
+			this.state.data[0].children.map((val) => {
+				if (val.checked) {
+					counts++;
+					if (counts == childrens) {
+						// this.state.data[0] = {...this.state.data[0], checked: true};
+						this.state.data[0].checked = true;
+					}
+				}
+			})
+		}
 	}
 
 	// var dataSource = Object.create(this.state.data);
