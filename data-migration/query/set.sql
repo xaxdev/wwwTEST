738286c348570d6cb85bfe771bcc981b28ab7fda
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-04-04 18:27:07 +0700

    Added more data for set reference data.

--- data-migration/query/set.sql
+++ data-migration/query/set.sql
@@ -4,11 +4,19 @@ SELECT jewelry.[SetReference] AS 'setReference'
       ,item.[Id] AS 'id'
       ,item.[Reference] AS 'reference'
       ,item.[Price] AS 'price'
+	  ,item.[ActualCost] AS 'actualCost'
+	  ,item.[UpdatedCost] AS 'updatedCost'
+	  ,item.[Markup] AS 'markup'
+	  ,item.[WarehouseName] AS 'warehouseName'
       ,item.[Currency] AS 'currency'
+	  ,item.[SKU] AS 'sku'
+	  ,item.[VendorItemReferance] AS 'venderReference'
+	  ,ISNULL(company.[Name], '') AS 'companyName'
       ,ISNULL(setimg.[FILENAME], '') AS 'setImageName'
       ,ISNULL(setimg.[FILETYPE], '') AS 'setImageType'
       ,ISNULL(img.[FILENAME], '') AS 'imageName'
       ,ISNULL(img.[FILETYPE], '') AS 'imageType'
+	  ,item.[CreatedDate] AS 'createdDate'
 FROM [ITORAMA].[dbo].[Jewelry] jewelry
 INNER JOIN [ITORAMA].[dbo].[Items] item
   ON item.[Reference] = jewelry.[ItemReference]
@@ -21,5 +29,7 @@ LEFT JOIN [ITORAMA].[dbo].[ItemImages] img
   ON item.[Id] = img.[ITEMRECID]
   AND item.[Company] = img.[Company]
   AND img.[TYPEID] = 'Image'
+LEFT JOIN [ITORAMA].[dbo].[Company] company
+  ON item.[Company] = company.[Code]
 WHERE jewelry.[SetReference] <> ''
 ORDER BY jewelry.[SetReference], item.[Id]
--- data-migration/src/lib/migration/set.js
+++ data-migration/src/lib/migration/set.js
@@ -11,14 +11,24 @@ const getitemSets = async (index, exchangeRates) => {
         const query = await file.read(Path.resolve(constant.SET_QUERY))
         const records = await db.exec(query, config.db)
         const itemSets = [];
+        let description = [];
         let reference = '';
         for (let record of records) {
             if (record.setReference !== reference) {
+                description = []
                 reference = record.setReference
+                // console.log(description.join());
                 const itemSet = {
                     reference: record.setReference,
+                    description: '',
                     items: [],
-                    totalPrice: {}
+                    totalActualCost: {},
+                    totalUpdatedCost: {},
+                    totalPrice: {},
+                    markup: record.markup,
+                    company: record.companyName,
+                    warehouse: record.warehouseName,
+                    createdDate: record.createdDate
                 }
 
                 if (!!record.setImageName && !!record.setImageType) {
@@ -30,16 +40,26 @@ const getitemSets = async (index, exchangeRates) => {
 
                 itemSets.push(itemSet)
             }
+            description.push(record.reference);
 
             const current = itemSets[itemSets.length - 1]
             const item = {
                 id: record.id,
+                description: description.join(),
                 reference: record.reference,
                 image: {
                     original: `${config.gallery.original}/${record.imageName}.${record.imageType}`,
                     thumbnail: `${config.gallery.thumbnail}/${record.imageName}.${record.imageType}`
                 },
-                price: {}
+                price: {},
+                actualCost: {},
+                updatedCost: {},
+                markup: record.markup,
+                company: record.companyName,
+                warehouse: record.warehouseName,
+                venderReference: record.venderReference,
+                sku: record.sku,
+                createdDate: record.createdDate
             }
 
             const exchangeRateFromUSDToHomeCurrency = exchangeRates.filter(exchangeRate => exchangeRate.from === 'USD' && exchangeRate.to === record.currency)[0];
@@ -48,18 +68,30 @@ const getitemSets = async (index, exchangeRates) => {
             // price in other currencies
             for (let rate of rates) {
                 item.price[rate.to] = record.price * rate.exchangeRate / 100;
-                current.totalPrice[rate.to] = (current.totalPrice[rate.to] || 0) + item.price[rate.to]
+                item.actualCost[rate.to] = record.actualCost * rate.exchangeRate / 100;
+                item.updatedCost[rate.to] = record.updatedCost * rate.exchangeRate / 100;
+                current.totalPrice[rate.to] = (current.totalPrice[rate.to] || 0) + item.price[rate.to];
+                current.totalActualCost[rate.to] = (current.totalActualCost[rate.to] || 0) + item.actualCost[rate.to];
+                current.totalUpdatedCost[rate.to] = (current.totalUpdatedCost[rate.to] || 0) + item.updatedCost[rate.to];
             }
 
             // price in USD
             if (!!exchangeRateFromUSDToHomeCurrency) {
-                item.price.USD = record.price * 100 / exchangeRateFromUSDToHomeCurrency.exchangeRate
-                current.totalPrice.USD = (current.totalPrice.USD || 0) + item.price.USD
+                item.price.USD = record.price * 100 / exchangeRateFromUSDToHomeCurrency.exchangeRate;
+                item.actualCost.USD = record.actualCost * 100 / exchangeRateFromUSDToHomeCurrency.exchangeRate;
+                item.updatedCost.USD = record.updatedCost * 100 / exchangeRateFromUSDToHomeCurrency.exchangeRate;
+                current.totalPrice.USD = (current.totalPrice.USD || 0) + item.price.USD;
+                current.totalActualCost.USD = (current.totalActualCost.USD || 0) + item.actualCost.USD;
+                current.totalUpdatedCost.USD = (current.totalUpdatedCost.USD || 0) + item.updatedCost.USD
             }
 
             // price in home currency
             item.price[record.currency] = record.price;
-            current.totalPrice[record.currency] = (current.totalPrice[record.currency] || 0) + item.price[record.currency]
+            item.actualCost[record.currency] = record.actualCost;
+            item.updatedCost[record.currency] = record.updatedCost;
+            current.totalPrice[record.currency] = (current.totalPrice[record.currency] || 0) + item.price[record.currency];
+            current.totalActualCost[record.currency] = (current.totalActualCost[record.currency] || 0) + item.actualCost[record.currency]
+            current.totalUpdatedCost[record.currency] = (current.totalUpdatedCost[record.currency] || 0) + item.updatedCost[record.currency]
 
             current.items.push(item)
         }
