8a4feb12e5b91742820bcb55edaf758d3f892805
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-04-25 10:37:42 +0700

    Fixed flot 3 digit for carat.

--- web/code/plugins/http/src/components/productdetail/productRawmaterialAttributes.js
+++ web/code/plugins/http/src/components/productdetail/productRawmaterialAttributes.js
@@ -3,6 +3,7 @@ import { Table } from 'react-bootstrap';
 import { Link } from 'react-router';
 import convertBlanktodash  from '../../utils/convertBlanktodash';
 import numberFormat from '../../utils/convertNumberformatwithcomma2digit';
+import numberFormat3 from '../../utils/convertNumberformatwithcomma3digit';
 import convertDate from '../../utils/convertDate';
 import checkInarray from '../../utils/checkInarray';
 const pructdetailurl = '/productreletedetail/';
@@ -44,7 +45,7 @@ const Gemstoneattr =  (props) =>{
                  return (
                    <tr key={index}>
                      <td title="Stone Type" className="text-center">{convertBlanktodash(data.stoneTypeName)}</td>
-                     <td title="Total Carat Weight" className="text-center">{numberFormat(data.carat)}</td>
+                     <td title="Total Carat Weight" className="text-center">{numberFormat3(data.carat)}</td>
                      </tr>
                 )
               }
