d353b61d6fe49bf5d850dac20beadbb22d66befa
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-04-28 16:58:40 +0700

    Fixed unit all items for setitem on search result page.

--- web/code/plugins/http/src/containers/searchresults/search_reslts.js
+++ web/code/plugins/http/src/containers/searchresults/search_reslts.js
@@ -558,7 +558,7 @@ class SearchResult extends Component {
     return(
       <div>
         <div id="dvTotalsub1" className="bg-or text-center">
-            <span><span className="font-b fc-000">Total Items :</span> <span className="font-w9">{ numberFormat(allItems.length) } Items </span><span className="padding-lf15">|</span></span>
+            <span><span className="font-b fc-000">Total Items :</span> <span className="font-w9">{ numberFormat(allItems.length) } {ViewAsSet ? 'Sets' : 'Items'} </span><span className="padding-lf15">|</span></span>
             <span className={`${(userLogin.permission.price == 'Public' || userLogin.permission.price == 'Updated'
                 || userLogin.permission.price == 'All') ?
                 '' : 'hidden'}`}>
