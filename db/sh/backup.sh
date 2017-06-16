232e9aa73db4070f3f4acfdc26887d0bc0301f58
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-06-16 14:19:17 +0700

    Modify command upload to s3.

--- db/sh/backup.sh
+++ db/sh/backup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-ROOT="/data/backup/mongodb"
+ROOT="./data/backup/mongodb"
 EXTENSION=".gz"
 
-PATH=${ROOT}/$(date +"%Y/%m")
+FOLDER=$(/bin/date +"%Y/%m")
+PATH=${ROOT}/${FOLDER}
 FILE=${PATH}/$(/bin/date +"%Y%m%d_%H%m")${EXTENSION}
+
 
 /bin/mkdir ${PATH} -p
 
 /usr/bin/mongodump -h 192.168.1.93:27017 -d mol_development --archive=${FILE} --gzip
 
-aws s3 cp s3://mouawad/mol/backup/mongodb/data/ ./data/backup/mongodb/${FILE}
+/usr/bin/aws s3 cp ${FILE} s3://mouawad/mol/backup/mongodb/data/${FOLDER}/
