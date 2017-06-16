a2c18b2d04eddacf6976f962c653ca02627ced94
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-06-16 11:27:41 +0700

    Changed folder backup file db.

--- db/sh/backup.sh
+++ db/sh/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ROOT="/mnt/backup/mongodb"
+ROOT="/data/backup/mongodb"
 EXTENSION=".gz"
 
 PATH=${ROOT}/$(date +"%Y/%m")
@@ -9,3 +9,5 @@ FILE=${PATH}/$(/bin/date +"%Y%m%d_%H%m")${EXTENSION}
 /bin/mkdir ${PATH} -p
 
 /usr/bin/mongodump -h 192.168.1.93:27017 -d mol_development --archive=${FILE} --gzip
+
+aws s3 cp s3://mouawad/mol/backup/mongodb/data/ ./data/backup/mongodb/${FILE}
