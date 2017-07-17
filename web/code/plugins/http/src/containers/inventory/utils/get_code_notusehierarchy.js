7e0800d9f20749b834d9fa1e485c8caa9d8f1bea
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-07-17 16:49:17 +0700

    Fixed check notUseHierarchy is null on load page.

--- web/code/plugins/http/src/containers/inventory/utils/get_code_notusehierarchy.js
+++ web/code/plugins/http/src/containers/inventory/utils/get_code_notusehierarchy.js
@@ -1,10 +1,12 @@
 export default function GetCodeNotUseHierarchy(notUseHierarchy, category){
     let code = '';
-    if (notUseHierarchy[category] != undefined && notUseHierarchy[category].length > 0) {
-        notUseHierarchy[category].map((val) => {
-            code = (code == '') ? val.code : code + '|' + val.code;
-        });
-        // filters.push({'notUseHierarchy': code});
+    if (notUseHierarchy != null) {
+        if (notUseHierarchy[category] != undefined && notUseHierarchy[category].length > 0) {
+            notUseHierarchy[category].map((val) => {
+                code = (code == '') ? val.code : code + '|' + val.code;
+            });
+            // filters.push({'notUseHierarchy': code});
+        }
     }
     return code;
 }
