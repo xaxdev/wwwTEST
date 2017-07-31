c9bdc94d851434b1d45c5d710c0af272d3f07514
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-07-31 11:47:11 +0700

    Edited wording Copyright and fixed selected env for selected images for pdf print out.

--- web/code/plugins/http/src/utils/genTemplatePdfMyCatalog.js
+++ web/code/plugins/http/src/utils/genTemplatePdfMyCatalog.js
@@ -6,7 +6,7 @@ export default function GenTemplateHtml(ROOT_URL, imagesReplace, dv){
     const userLogin = dv.userLogin;
     const printPage = dv.printPage.value;
     const printPrice = dv.printPrice.value;
-    const env = process.env.NODE_ENV || 'development';
+    const env = dv.env;
     let dvTotalItems = dv.dvTotalItems;
     let dvTotalSetItems = dv.dvTotalSetItems;
     let dvGridview = dv.dvGridview;
@@ -32,6 +32,7 @@ export default function GenTemplateHtml(ROOT_URL, imagesReplace, dv){
     let stylePrice = 'color: #ae8f3b; font-weight: bold;';
     let thumbnaillgrid = 'margin: 0 auto; height: 200px; overflow: hidden; position: relative; width:123px;';
     let thumbnaillgridimg = 'width:120px;margin: 0 auto;';
+    // let imgPath = 'file:///var/www/mol/web/code/plugins/http/public/images/';
     let imgPath = env == 'production'
                             ? 'file:///var/www/mol/web/code/plugins/http/public/images/'
                             : 'file:///home/dev/www/mol/web/code/plugins/http/public/images/';
