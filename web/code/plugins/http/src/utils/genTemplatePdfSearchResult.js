c2757c449e350e0847489823ff655603d9d8791c
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-08-11 18:04:03 +0700

    Fixed css.

--- web/code/plugins/http/src/utils/genTemplatePdfSearchResult.js
+++ web/code/plugins/http/src/utils/genTemplatePdfSearchResult.js
@@ -87,7 +87,7 @@ export default function GenTemplateHtml(showGridView, showListView, ROOT_URL, im
     dvListview = dvListview.replace(/<thead/g,'<thead style="padding:10px 10px; text-align:center; color:#fff; background-color: #383735;  font-weight: normal; font-size: 8px;"');
     dvListview = dvListview.replace(/<th role="columnheader" scope="col"/g,'<th style="padding:10px 10px; text-align:center; color:#fff; background-color: #383735;  font-weight: normal; font-size: 8px; border: 1px solid #5c5954;" role="columnheader" scope="col"');
     // dvListview = dvListview.replace(/<td/g,'<td style="padding:5px 5px;word-break: normal;font-size: 8px; border: 1px solid #5c5954;" ');
-    dvListview = dvListview.replace(/<th/g,'<td style="border:1px solid #5c5954;padding:5px 5px;word-break: normal;font-size: 8px; border: 1px solid #5c5954;" ');
+    // dvListview = dvListview.replace(/<th/g,'<td style="border:1px solid #5c5954;padding:5px 5px;word-break: normal;font-size: 8px; border: 1px solid #5c5954;" ');
     dvListview = dvListview.replace(/class="tdd"/g,'style="padding:5px 5px;word-break: normal;font-size: 8px; border: 1px solid #5c5954;"');
     dvListview = dvListview.replace(/class="bd-lb-white"/g,'style="border-left: 1px solid #fff;border-bottom: 1px solid #fff;padding:5px 5px;word-break: normal;font-size: 8px;"');
     dvListview = dvListview.replace(/class="font-b fc-000 text-center bg-eb"/g,`style="font-weight: bold; font-family:'${'open_sanssemibold'}';color:#000;text-align: center;background-color: #ebd79a;padding:5px 5px;word-break: normal;font-size: 8px; border: 1px solid #5c5954;border-right: transparent; border-bottom: 1px solid #5c5954;"`);
@@ -104,7 +104,7 @@ export default function GenTemplateHtml(showGridView, showListView, ROOT_URL, im
     dvListviewAll = dvListviewAll.replace(/<thead/g,'<thead style="padding:10px 10px; text-align:center; color:#fff; background-color: #383735;  font-weight: normal; font-size: 8px;"');
     dvListviewAll = dvListviewAll.replace(/<th role="columnheader" scope="col"/g,'<th style="border: 1px solid #5c5954; padding:10px 10px; text-align:center; color:#fff; background-color: #383735;  font-weight: normal; font-size: 8px; border: 1px solid #5c5954;" role="columnheader" scope="col"');
     // dvListviewAll = dvListviewAll.replace(/<td/g,'<td style="padding:5px 5px;word-break: normal;font-size: 8px; border: 1px solid #5c5954;" ');
-    dvListviewAll = dvListviewAll.replace(/<th/g,'<td style="border:1px solid #5c5954;padding:5px 5px;word-break: normal;font-size: 8px; border: 1px solid #5c5954;" ');
+    // dvListviewAll = dvListviewAll.replace(/<th/g,'<td style="border:1px solid #5c5954;padding:5px 5px;word-break: normal;font-size: 8px; border: 1px solid #5c5954;" ');
     dvListviewAll = dvListviewAll.replace(/class="tdd"/g,'style="padding:5px 5px;word-break: normal;font-size: 8px; border: 1px solid #5c5954; border-right: transparent; border-bottom: transparent;"');
     dvListviewAll = dvListviewAll.replace(/class="bd-lb-white"/g,'style="border-top:1px solid #5c5954; border-left: 1px solid #fff;border-bottom: 1px solid #fff;padding:5px 5px;word-break: normal;font-size: 8px;"');
     dvListviewAll = dvListviewAll.replace(/class="font-b fc-000 text-center bg-eb"/g,`style="font-weight: bold; font-family:'${'open_sanssemibold'}';color:#000;text-align: center;background-color: #ebd79a;padding:5px 5px;word-break: normal;font-size: 8px; border: 1px solid #5c5954;border-right: transparent; border-bottom: 1px solid #5c5954;"`);
