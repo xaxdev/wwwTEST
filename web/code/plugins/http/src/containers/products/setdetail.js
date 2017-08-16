a9adcf478cc4d496f58ec72db39162b3aedf347c
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-08-16 14:28:03 +0700

    Fixed show currency on set image detail and sorting of setdetail nav.

--- web/code/plugins/http/src/containers/products/setdetail.js
+++ web/code/plugins/http/src/containers/products/setdetail.js
@@ -442,7 +442,7 @@ class productdetail extends Component {
             const currency = logindata.currency;
 
             jQuery('#galleryimgset').attr('src',activegallery);
-            jQuery('#showtotal').text('Total Public Price (Set): '+numberFormat(totalprice)+' '+currency);
+            jQuery('#showtotal').text('Total Public Price (Set): '+numberFormat(totalprice)+' '+'USD');
             let rotatecount = 0;
             jQuery('#btnupset').click(function(){
               jQuery('#galleryimgset').css({'-webkit-transform': 'rotate('+(rotatecount+=90)+'deg)'});
