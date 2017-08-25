6500881d24e07a2464c42563b4411b7ed34a1e04
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-08-25 10:17:14 +0700

    Test sum of the price * quantity for all items.

--- web/code/tests/cart_summary.js
+++ web/code/tests/cart_summary.js
@@ -1,6 +1,13 @@
-function CartSummary() {}
+function CartSummary(items) {
+  this._items = items;
+}
 
 CartSummary.prototype.getSubtotal = function() {
+    if (this._items.length) {
+        return this._items.reduce(function(subtotal, item) {
+            return subtotal += (item.quantity * item.price);
+        }, 0);
+    }
     return 0;
 };
 
--- web/code/tests/cart_summary_test.js
+++ web/code/tests/cart_summary_test.js
@@ -3,8 +3,12 @@ var expect = chai.expect; // we are using the "expect" style of Chai
 var CartSummary = require('./cart_summary');
 
 describe('CartSummary', function() {
-    it('getSubtotal() should return 0 if no items are passed in', function() {
-          var cartSummary = new CartSummary([]);
-          expect(cartSummary.getSubtotal()).to.equal(0);
+    it('getSubtotal() should return the sum of the price * quantity for all items', function() {
+          var cartSummary = new CartSummary([
+              { id: 1, quantity: 4, price: 50 },
+              { id: 2, quantity: 2, price: 30 },
+              { id: 3, quantity: 1, price: 40 }
+          ]);
+          expect(cartSummary.getSubtotal()).to.equal(300);
     });
 });
