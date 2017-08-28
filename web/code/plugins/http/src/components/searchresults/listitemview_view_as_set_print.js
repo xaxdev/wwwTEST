a261fcf5047222215725e4215092ffb71d8ca385
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-08-28 16:06:52 +0700

    Fixed print pdf.

--- web/code/plugins/http/src/components/searchresults/listitemview_view_as_set_print.js
+++ web/code/plugins/http/src/components/searchresults/listitemview_view_as_set_print.js
@@ -18,53 +18,97 @@ class ListItemsViewASSetPrint extends Component {
          let row = item.items.length +1;
          const userLogin = JSON.parse(sessionStorage.logindata);
 
-         return (
-             <tbody>
-               <td className="tdd" rowSpan={row}><img src={item.imageThumbnail} width="60"/></td>
-               <td className="tdd" rowSpan={row}>{item.reference}</td>
-                {item.items.map((subitem) => {
-                    return (
-                        <tr>
-                            <td className="tdd">{subitem.reference}</td>
-                            <td className="tdd">{subitem.description}</td>
-                            <td className="tdd">{subitem.sku}</td>
-                            <td className="tdd">{subitem.hierarchy != undefined ? subitem.hierarchy.split('\\').slice(-1).pop():''}</td>
-                            <td className="tdd">{subitem.company}</td>
-                            <td className="tdd">{subitem.warehouse}</td>
-                            <td className="tdd">{numberFormat2digit(subitem.grossWeight)}</td>
-                            <td className={`tdd ${(userLogin.permission.price == 'All') ?
-                                '' : 'hidden'}`}>{numberFormat(subitem.actualCost['USD'])}</td>
-                            <td className={`tdd ${(userLogin.permission.price == 'Updated'
-                                || userLogin.permission.price == 'All') ?
-                                '' : 'hidden'}`}>{numberFormat(subitem.updatedCost['USD'])}</td>
-                            <td className={`tdd ${(userLogin.permission.price == 'Public'
-                                || userLogin.permission.price == 'Updated'
-                                || userLogin.permission.price == 'All') ?
-                                '' : 'hidden'}`}>{numberFormat(subitem.price['USD'])}</td>
-                        </tr>
-                    );
-                })}
-                <tr>
-                    <td  colSpan="8" className="bd-lb-white"></td>
-                    <td className="font-b fc-000 text-center bg-eb">Total</td>
-                    <td className={`font-b fc-000 text-right bg-eb
-                        ${(userLogin.permission.price == 'All') ?
-                        '' : 'hidden'}`}>{numberFormat(item.totalActualCost['USD'])}</td>
-                    <td className={`font-b fc-000 text-right bg-eb
-                        ${(userLogin.permission.price == 'Updated'
-                        || userLogin.permission.price == 'All') ?
-                        '' : 'hidden'}`}>{numberFormat(item.totalUpdatedCost['USD'])}</td>
-                    <td className={`font-b fc-000 text-right bg-eb
-                        ${(userLogin.permission.price == 'Public'
-                        || userLogin.permission.price == 'Updated'
-                        || userLogin.permission.price == 'All') ?
-                        '' : 'hidden'}`}>{numberFormat(item.totalPrice['USD'])}</td>
-                </tr>
-                <tr>
-                  <td className="bd-tblr-white" colSpan="12" height="40px" ></td>
-                </tr>
-             </tbody>
-         );
+         if (item.items.length == 1) {
+             return (
+                 <tbody>
+                     {item.items.map((subitem) => {
+                         return (
+                             <tr>
+                                <td className="tdd"><img src={item.imageThumbnail} width="60"/></td>
+                                <td className="tdd">{item.reference}</td>
+                                 <td className="tdd">{subitem.reference}</td>
+                                 <td className="tdd">{subitem.description}</td>
+                                 <td className="tdd">{subitem.sku}</td>
+                                 <td className="tdd">{subitem.hierarchy != undefined ? subitem.hierarchy.split('\\').slice(-1).pop():''}</td>
+                                 <td className="tdd">{subitem.company}</td>
+                                 <td className="tdd">{subitem.warehouse}</td>
+                                 <td className="tdd">{numberFormat2digit(subitem.grossWeight)}</td>
+                                 <td className={`tdd${(userLogin.permission.price == 'All') ?
+                                    '' : 'hidden'}`}>{numberFormat(subitem.actualCost['USD'])}</td>
+                                 <td className={`tdd${(userLogin.permission.price == 'Updated'
+                                     || userLogin.permission.price == 'All') ?
+                                     '' : 'hidden'}`}>{numberFormat(subitem.updatedCost['USD'])}</td>
+                                 <td className={`tdd${(userLogin.permission.price == 'Public'
+                                     || userLogin.permission.price == 'Updated'
+                                     || userLogin.permission.price == 'All') ?
+                                     '' : 'hidden'}`}>{numberFormat(subitem.price['USD'])}</td>
+                             </tr>
+                         );
+                     })}
+                     <tr>
+                         <td colSpan="8" className="bd-lb-white"></td>
+                         <td className="font-b fc-000 text-center bg-eb">Total</td>
+                         <td className={`font-b fc-000 text-right bg-eb${(userLogin.permission.price == 'All') ?
+                            '' : 'hidden'}`}>{numberFormat(item.totalActualCost['USD'])}</td>
+                         <td className={`font-b fc-000 text-right bg-eb${(userLogin.permission.price == 'Updated'
+                             || userLogin.permission.price == 'All') ?
+                             '' : 'hidden'}`}>{numberFormat(item.totalUpdatedCost['USD'])}</td>
+                         <td className={`font-b fc-000 text-right bg-eb${(userLogin.permission.price == 'Public'
+                             || userLogin.permission.price == 'Updated'
+                             || userLogin.permission.price == 'All') ?
+                             '' : 'hidden'}`}>{numberFormat(item.totalPrice['USD'])}</td>
+                     </tr>
+                     <tr>
+                        <td className="bd-tblr-white" colSpan="12" height="40px" ></td>
+                     </tr>
+                 </tbody>
+             );
+         }else{
+             return (
+                 <tbody>
+                     <td className="tdd" rowSpan={row}><img src={item.imageThumbnail} width="60"/></td>
+                     <td className="tdd" rowSpan={row}>{item.reference}</td>
+                     {item.items.map((subitem) => {
+                         return (
+                             <tr>
+                                 <td className="tdd">{subitem.reference}</td>
+                                 <td className="tdd">{subitem.description}</td>
+                                 <td className="tdd">{subitem.sku}</td>
+                                 <td className="tdd">{subitem.hierarchy != undefined ? subitem.hierarchy.split('\\').slice(-1).pop():''}</td>
+                                 <td className="tdd">{subitem.company}</td>
+                                 <td className="tdd">{subitem.warehouse}</td>
+                                 <td className="tdd">{numberFormat2digit(subitem.grossWeight)}</td>
+                                 <td className={`tdd${(userLogin.permission.price == 'All') ?
+                                    '' : 'hidden'}`}>{numberFormat(subitem.actualCost['USD'])}</td>
+                                 <td className={`tdd${(userLogin.permission.price == 'Updated'
+                                     || userLogin.permission.price == 'All') ?
+                                     '' : 'hidden'}`}>{numberFormat(subitem.updatedCost['USD'])}</td>
+                                 <td className={`tdd${(userLogin.permission.price == 'Public'
+                                     || userLogin.permission.price == 'Updated'
+                                     || userLogin.permission.price == 'All') ?
+                                     '' : 'hidden'}`}>{numberFormat(subitem.price['USD'])}</td>
+                             </tr>
+                         );
+                     })}
+                     <tr>
+                         <td  colSpan="8" className="bd-lb-white"></td>
+                         <td className="font-b fc-000 text-center bg-eb">Total</td>
+                         <td className={`font-b fc-000 text-right bg-eb${(userLogin.permission.price == 'All') ?
+                            '' : 'hidden'}`}>{numberFormat(item.totalActualCost['USD'])}</td>
+                         <td className={`font-b fc-000 text-right bg-eb${(userLogin.permission.price == 'Updated'
+                             || userLogin.permission.price == 'All') ?
+                             '' : 'hidden'}`}>{numberFormat(item.totalUpdatedCost['USD'])}</td>
+                         <td className={`font-b fc-000 text-right bg-eb${(userLogin.permission.price == 'Public'
+                             || userLogin.permission.price == 'Updated'
+                             || userLogin.permission.price == 'All') ?
+                             '' : 'hidden'}`}>{numberFormat(item.totalPrice['USD'])}</td>
+                     </tr>
+                     <tr>
+                        <td className="bd-tblr-white" colSpan="12" height="40px" ></td>
+                     </tr>
+                 </tbody>
+             );
+         }
      }
 }
 
