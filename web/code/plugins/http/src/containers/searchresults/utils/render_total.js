302e0a23aa151f55b629059e9f9cb1da514e2b7d
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-08-29 10:10:30 +0700

    Fixed hide |.

--- web/code/plugins/http/src/containers/searchresults/utils/render_total.js
+++ web/code/plugins/http/src/containers/searchresults/utils/render_total.js
@@ -9,24 +9,41 @@ class RenderClassTotals extends Component {
         return(
           <div>
             <div id="dvTotalsub1" className="bg-or text-center">
-                <span><span className="font-b fc-000">Total Items :</span> <span className="font-w9">{ numberFormat(allItems.length) } {ViewAsSet ? 'Sets' : 'Items'} </span><span className="padding-lf15">|</span></span>
-                <span className={`${(userLogin.permission.price == 'Public' || userLogin.permission.price == 'Updated'
+                <span>
+                    <span className="font-b fc-000">Total Items :</span>
+                    <span className="font-w9">{ numberFormat(allItems.length) } {ViewAsSet ? 'Sets' : 'Items'} </span>
+                    <span className="padding-lf15">|</span>
+                </span>
+                <span className={`${(userLogin.permission.price == 'Public'
+                    || userLogin.permission.price == 'Updated'
                     || userLogin.permission.price == 'All') ?
                     '' : 'hidden'}`}>
-                    <span className="font-b fc-000">Total Public Price :</span> <span className="font-w9">{ _totalPublicPrice } { ViewAsSet ? 'USD' : userLogin.currency }</span><span className="padding-lf15">
-                    |
-                    </span>
+                    <span className="font-b fc-000">Total Public Price :</span>
+                    <span className="font-w9">{ _totalPublicPrice } { ViewAsSet ? 'USD' : userLogin.currency }</span>
                 </span>
-                <span className={`${(userLogin.permission.price == 'Updated' || userLogin.permission.price == 'All') ?
+                <span className={`${(userLogin.permission.price == 'Updated'
+                    || userLogin.permission.price == 'All') ?
                     '' : 'hidden'}`}>
-                    <span className="font-b fc-000">Total Updated Cost :</span> <span className="font-w9">{ _totalUpdatedCost } { ViewAsSet ? 'USD' : userLogin.currency }
-                    </span>
+                    <span className="padding-lf15">|</span>
+                    <span className="font-b fc-000">Total Updated Cost :</span>
+                    <span className="font-w9">{ _totalUpdatedCost } { ViewAsSet ? 'USD' : userLogin.currency }</span>
                 </span>
             </div>
             <div id="dvTotalsub2" className="bg-f7d886 text-center">
-                <span><span className="font-b fc-000">Highest Price :</span> <span className="font-w9">{ numberFormat(maxPrice) } { ViewAsSet ? 'USD' : userLogin.currency } </span><span className="padding-lf15">|</span></span>
-                <span><span className="font-b fc-000">Lowest Price :</span> <span className="font-w9">{ numberFormat(minPrice) } { ViewAsSet ? 'USD' : userLogin.currency } </span><span className="padding-lf15">|</span></span>
-                <span><span className="font-b fc-000">Average Price :</span> <span className="font-w9">{ numberFormat(avrgPrice) } { ViewAsSet ? 'USD' : userLogin.currency } </span></span>
+                <span>
+                    <span className="font-b fc-000">Highest Price :</span>
+                    <span className="font-w9">{ numberFormat(maxPrice) } { ViewAsSet ? 'USD' : userLogin.currency } </span>
+                    <span className="padding-lf15">|</span>
+                </span>
+                <span>
+                    <span className="font-b fc-000">Lowest Price :</span>
+                    <span className="font-w9">{ numberFormat(minPrice) } { ViewAsSet ? 'USD' : userLogin.currency } </span>
+                    <span className="padding-lf15">|</span>
+                </span>
+                <span>
+                    <span className="font-b fc-000">Average Price :</span>
+                    <span className="font-w9">{ numberFormat(avrgPrice) } { ViewAsSet ? 'USD' : userLogin.currency } </span>
+                </span>
             </div>
           </div>
         );
