e6838e12ea0fd1e573ffab967bbe09fd17c5746f
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2017-09-18 11:16:05 +0700

    Test stub.

--- web/code/tests/cart_summary_test.js
+++ web/code/tests/cart_summary_test.js
@@ -15,32 +15,30 @@ describe('CartSummary', function() {
     });
 });
 
-describe('getTax()', function() {
-    beforeEach(function() {
-        console.log('beforeEach-->');
-        sinon.stub(tax, 'calculate', function(subtotal, state, done) {
-            setTimeout(function() {
-                done({
-                    amount: 30
-                });
-            }, 0);
-        });
-    });
-
-    afterEach(function() {
-        console.log('afterEach-->');
-        tax.calculate.restore();
-    });
-
-    it('get Tax() should execute the callback function with the tax amount', function(done) {
-        var cartSummary = new CartSummary([
-            { id: 1, quantity: 4, price: 50 },
-            { id: 2, quantity: 2, price: 30 },
-            { id: 3, quantity: 1, price: 40 }
-        ]);
-        cartSummary.getTax('NY', function(taxAmount) {
-            expect(taxAmount).to.equal(30);
-            done();
-        });
-    });
-});
+// describe('getTax()', function() {
+//     beforeEach(function() {
+//         sinon.stub(tax, 'calculate', function(subtotal, state, done) {
+//             setTimeout(function() {
+//                 done({
+//                     amount: 300
+//                 });
+//             }, 0);
+//         });
+//     });
+//
+//     afterEach(function() {
+//         tax.calculate.restore();
+//     });
+//
+//     it('get Tax() should execute the callback function with the tax amount', function(done) {
+//         var tax = new tax([
+//             { id: 1, quantity: 4, price: 50 },
+//             { id: 2, quantity: 2, price: 30 },
+//             { id: 3, quantity: 1, price: 40 }
+//         ]);
+//         tax.getTax('NY', function(taxAmount) {
+//             expect(taxAmount).to.equal(300);
+//             done();
+//         });
+//     });
+// });
--- web/code/tests/tax.js
+++ web/code/tests/tax.js
@@ -1,15 +1,17 @@
-module.exports = {
-    calculate: function(subtotal, state, done) {
-        console.log('getTax-->');
-        // implemented later or in parallel by our coworker
-    }
+// module.exports = {
+//     calculate: function(subtotal, state, done) {
+//         console.log('getTax-->');
+//         // implemented later or in parallel by our coworker
+//     }
+// };
+function CartSummary(items) {
+    this._items = items;
+}
+
+CartSummary.prototype.getTax = function(state, done) {
+    tax.calculate(this.getSubtotal(), state, function(taxInfo) {
+        done(taxInfo.amount);
+    });
 };
 
-// CartSummary.prototype.getTax = function(state, done) {
-//     console.log('getTax-->');
-//     tax.calculate(this.getSubtotal(), state, function(taxInfo) {
-//         done(taxInfo.amount);
-//     });
-// };
-//
-// module.exports = CartSummary;
+module.exports = CartSummary;
