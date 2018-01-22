9a8f3e3ce8c006a13c9a33f77a9899954ac2b212
korakod chaisongkram <korakod.chaisongkram@itorama.com>
2018-01-22 16:23:39 +0700

    Added test function and test script in to project.

---
+++ web/code/tests/client.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const request = require('request-promise-native')
+const _ = require('lodash')
+
+const PRODUCTS_SERVICE_URL = process.env.PRODUCTS_SERVICE_URL || 'http://localhost:1234';
+console.log('PRODUCTS_SERVICE_URL-->',PRODUCTS_SERVICE_URL);
+
+// async function getAllProducts () {
+//   const products = await request(`${PRODUCTS_SERVICE_URL}/products`)
+//     .then(JSON.parse)
+//
+//   const productsString = _.reduce(products, (logString, product) => `${logString} ${product.name}`, 'CLIENT: Current products are:')
+//
+//   console.log(productsString)
+// }
+
+function getProducts (query) {
+  return request({
+    uri: `${PRODUCTS_SERVICE_URL}/products`,
+    qs: query,
+    json: true
+  })
+}
+
+function registerProduct(product) {
+  return request.post({
+    url: `${PRODUCTS_SERVICE_URL}/products`,
+    body: product,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
+module.exports = {
+  //getAllProducts,
+  getProducts,
+  registerProduct
+}
---
+++ web/code/tests/integration.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const app = require('../server');
+const chai = require('chai');
+const request = require('supertest');
+const expect = chai.expect;
+
+describe('Todos list API Integration Tests', function() {
+
+});    
